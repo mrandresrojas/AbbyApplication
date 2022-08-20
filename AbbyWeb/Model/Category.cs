79aeef6227abcc730e0c5f9f0b2ee963b552a4c0
carlosa.rojas <carlosa.rojas@LSTKBO220889>
2022-08-19 19:19:33 -0500

    Agregar archivos de proyecto.

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display (Name ="Display order")]
+        [Range(1,100,ErrorMessage ="Display order must be in range 1-100")]
+
+        public int DisplayOrder { get; set; }
+
+
+    }
+}
---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories;
+    [BindProperties]
+
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+
+        
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost( ) 
+        {
+        if (Category.Name == Category.DisplayOrder.ToString()) {
+
+
+            ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name");
+
+
+        }
+        if (ModelState.IsValid) {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+        return Page();
+    }
+}
+
